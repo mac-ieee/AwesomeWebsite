87977aab55598fa96652d02cd6032a78f1ec8374
Ben Li <benji112358@gmail.com>
2020-01-21 18:29:58 -0500

    separate stylesheet for navbar

--- main.js
+++ main.js
@@ -2,13 +2,24 @@
 var cards = document.getElementsByClassName('cover-card');
 var branch_cards = document.getElementsByClassName('branch-card');
 
+var cover_title = document.getElementsByClassName('cover-title-heading');
+var cover_subheading = document.getElementsByClassName('cover-sub-heading');
+var cover_ctalink = document.getElementsByClassName('cover-cta-link');
+var cover_titleprice = document.getElementsByClassName('cover-title-price');
+
+
 var myScrollFunc = function () {
     var y = window.scrollY;
     if (y > 1) {
+        console.log("asd");
     	cards[0].style.opacity = 0.8;
 		cards[1].style.opacity = 0.8;
 		cards[2].style.opacity = 0.8;
 		cards[3].style.opacity = 0.8;
+
+        // make cover-text dissapear on scroll
+        cover_title[0].style.opacity = 0;
+
     }
 
     if (y > 700) {
@@ -19,5 +30,4 @@ var myScrollFunc = function () {
     }
 };
 
-
-window.addEventListener("scroll", myScrollFunc);
+document.addEventListener("scroll", myScrollFunc);
